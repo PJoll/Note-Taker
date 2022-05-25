f122875bfa63e16b59bd9ca0ee8fca0b146c2a80
Peter Jolley <peterjolley456@gmail.com>
2022-05-25 18:26:18 +0930

    app post and fileSync working

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var path = require("path");
-var fs = require ("fs");
+const fs = require ("fs");
 const { title } = require("process");
-const uuidv1 = require("uuid/v1");
+const uuidv1 = require("uuidv1");
 
 var PORT = process.env.PORT || 3001;
 
@@ -22,20 +22,22 @@ res.sendFile(path.join(__dirname, "/public/index.html")));
 
 
 app.get("/api/notes", (req, res) =>
-res.sendFile(path.join(__dirname, "/db/db.json")));
-// tried to create a unique note id usinig uuid
+res.sendFile(path.join(__dirname, "./db/db.json")));
+
+
 app.post("/api/notes", (req, res)=> {
+    // tried to create a unique note id usinig uuid
 const newNote = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv1(),
 }
-let notes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
-notes.push(newNote);
-fs.writeFileSync("/db/db.json", JSON.stringify(notes));
-res.json(notes)
 // need to convert notes to object no idea how
+let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+notes.push(newNote);
+fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 
+res.json(notes)
 });
 
 
